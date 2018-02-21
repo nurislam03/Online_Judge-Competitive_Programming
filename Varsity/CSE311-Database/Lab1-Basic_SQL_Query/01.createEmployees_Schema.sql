5a001ce8a5f6092d6e26d7f0de1d6cd9b538081c
Nur Islam <nurislam0333@gmail.com>
2018-02-22 00:23:39 +0600

    Basic sql | crereate Table

---
+++ Varsity/CSE311-Database/Lab1-Basic_SQL_Query/01.createEmployees_Schema.sql
@@ -0,0 +1,13 @@
+CREATE INTO Employees(
+    EMPLOYEE_ID int,
+    FIRST_NAME Varchar,
+    LAST_NAME Varchar,
+    EMAIL Varchar,
+    PHONE_NUMBER Varchar,
+    HIRE_DATE DATE,
+    JOB_ID Varchar,
+    SALARY Double,
+    COMMISSSION_PCT Double,
+    MANAGER_ID int,
+    DEPEARTMENT_ID int
+)
