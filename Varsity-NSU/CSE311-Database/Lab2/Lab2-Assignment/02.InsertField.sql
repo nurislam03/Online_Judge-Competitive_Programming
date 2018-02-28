6f22205e42659f48b086dac7db6c357eda08c00c
Nur Islam <nurislam0333@gmail.com>
2018-03-01 00:18:11 +0600

    change

--- Varsity-NSU/CSE311-Database/Lab2/Lab2-Assignment/02.InsertField.sql
+++ Varsity-NSU/CSE311-Database/Lab2/Lab2-Assignment/02.InsertField.sql
@@ -1,6 +1,6 @@
 Insert Into Field(Name, ID, Radius, Venue, Field_Type)
 Values('Pontal', 01, 120, 'Pontal (Walks)', 'Cycling'),
 ('Estádio', 02, 100, 'João Havelange', 'Football'),
-('Sambódromo', 02, 100, 'Sambódromo Marathon', 'Archery'),
-('National Stadium, Tokyo', 02, 100, 'João Havelange', 'Football'),
-('Estádio', 02, 100, 'opening/closing Ceremonies', 'Football')
+('Sambódromo', 03, 130, 'Sambódromo Marathon', 'Archery'),
+('National Stadium, Tokyo', 04, 110, 'João Havelange', 'Football'),
+('Estádio', 05, 90, 'opening/closing Ceremonies', 'Football')
