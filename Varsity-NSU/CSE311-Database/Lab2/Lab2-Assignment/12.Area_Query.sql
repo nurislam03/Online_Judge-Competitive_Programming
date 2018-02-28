9a0b8cb65605def91e7d19f57910adb5923c7474
Nur Islam <nurislam0333@gmail.com>
2018-03-01 01:36:27 +0600

    assignment | Lab-2

--- Varsity-NSU/CSE311-Database/Lab2/Lab2-Assignment/12.Area_Query.sql
+++ Varsity-NSU/CSE311-Database/Lab2/Lab2-Assignment/12.Area_Query.sql
@@ -1,3 +1,8 @@
 // 01
 Select (Length*Width) As area, (2*(Length+Width)) As Circumference, Sqrt((Length*Length) + (Width*Width)) As Diagonal
 From Area;
+
+
+// 02
+Select Concat(Name, " is situated in ", Location, " and the diagonal is ", Sqrt((Length*Length) + (Width*Width)))
+From Area;
