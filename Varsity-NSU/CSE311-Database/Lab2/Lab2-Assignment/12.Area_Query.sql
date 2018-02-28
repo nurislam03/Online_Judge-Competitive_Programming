d8aaa407b75e9dd562e3264031bd166e5c3a4bf3
Nur Islam <nurislam0333@gmail.com>
2018-03-01 01:31:56 +0600

    assignment | Lab-2

--- Varsity-NSU/CSE311-Database/Lab2/Lab2-Assignment/12.Area_Query.sql
+++ Varsity-NSU/CSE311-Database/Lab2/Lab2-Assignment/12.Area_Query.sql
@@ -1,3 +1,3 @@
 // 01
-Select (Length*Width) As area, (2*(Length+Width)) As Circumference, Sqrt((Length*Length) + (Width*Width))
+Select (Length*Width) As area, (2*(Length+Width)) As Circumference, Sqrt((Length*Length) + (Width*Width)) As Diagonal
 From Area;
