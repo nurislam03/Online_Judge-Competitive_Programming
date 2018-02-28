203fc74ab9bf7206417b8b838657cf8ad36b5622
Nur Islam <nurislam0333@gmail.com>
2018-03-01 00:16:25 +0600

    Lab2 | Assignment

---
+++ Varsity-NSU/CSE311-Database/Lab2/Lab2-Assignment/01.createField.sql
@@ -0,0 +1,7 @@
+Create Table Field(
+    Name varchar(40),
+    ID int,
+    Radius double,
+    Venue varchar(60),
+    Field_Type varchar(40)
+)
---
+++ Varsity-NSU/CSE311-Database/Lab2/Lab2-Assignment/02.InsertField.sql
@@ -0,0 +1,6 @@
+Insert Into Field(Name, ID, Radius, Venue, Field_Type)
+Values('Pontal', 01, 120, 'Pontal (Walks)', 'Cycling'),
+('Estádio', 02, 100, 'João Havelange', 'Football'),
+('Sambódromo', 02, 100, 'Sambódromo Marathon', 'Archery'),
+('National Stadium, Tokyo', 02, 100, 'João Havelange', 'Football'),
+('Estádio', 02, 100, 'opening/closing Ceremonies', 'Football')
