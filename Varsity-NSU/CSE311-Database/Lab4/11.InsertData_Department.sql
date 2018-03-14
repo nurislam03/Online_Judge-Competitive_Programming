515941d6b6a06e00f4afb2a87a21d75d0d4cb700
Nur Islam <nurislam0333@gmail.com>
2018-03-14 15:19:08 +0600

    CSE311 Lab - Basic SQL

---
+++ Varsity-NSU/CSE311-Database/Lab4/11.InsertData_Department.sql
@@ -0,0 +1,15 @@
+
+Insert Into Department(Did, Dname, Location, Budget, Branch_Number)
+Values(42, 'IT_MAN', 'Bogra', 800000, 1001),
+(52, 'IT_MAN', 'Rajshahi', 700000, 1201),
+(62, 'SA_MAN', 'Dhaka', 600000, 1301),
+(72, 'SA_REP', 'Bogra', 500000, 1202),
+(82, 'IT_MAN', 'Noakhali', 100000, 1203),
+
+
+Insert Into Department(Did, Dname, Location, Budget, Branch_Number)
+Values(92, 'IT_SUP', 'Bogra', 750000, 01),
+(12, 'IT_MAN', 'Rajshahi', 650000, 12),
+(42, 'SA_MAN', 'Dhaka', 550000, 18),
+(52, 'SA_REP', 'Bogra', 550000, 15),
+(42, 'IT_MAN', 'Noakhali', 100000, 13)
