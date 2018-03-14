149b7caf8b2a91f7308a888e677e8eac6496a266
Nur Islam <nurislam0333@gmail.com>
2018-03-14 15:18:04 +0600

    CSE311 Lab - Basic SQL

---
+++ Varsity-NSU/CSE311-Database/Lab4/02.InsertData_Employee.sql
@@ -0,0 +1,11 @@
+Insert Into Employee(EID, Ename, Salary, Location, Age, Hire_Date, Did)
+Values(01, 'Nur Islam', 50000, 'Dhaka', 21, '2018-01-01', 42),
+(02, 'Tasnuva', 45999, 'Noakhali', 34, '2017-05-04', 52),
+(03, 'Maksuda', 47999, 'Barisal', 40, '2000-05-04', 62),
+(04, 'Maxy', 39999, 'Feni', 30, '2010-05-04', 72),
+(05, 'Tabassum', 45999, 'Dhaka', 29, '2016-05-04', 82),
+(06, 'Xihan', 46999, 'Bogra', 22, '2009-05-11', 92),
+(07, 'Mayesha', 35999, 'Dhaka', 23, '2016-05-04', 12),
+(08, 'Alvee', 25999, 'Syllet', 21, '2016-07-24', 42),
+(09, 'Shahrear', 45999, 'Comilla', 27, '2016-03-14', 52),
+(10, 'Pretty', 49499, 'B.Baria', 19, '2016-05-03', 42)
