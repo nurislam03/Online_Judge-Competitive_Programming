2d40e2eff5ec9bc13304f6183cfbb56971f37f21
Nur Islam <nurislam0333@gmail.com>
2018-03-22 01:41:37 +0600

    Database (CSE311 |  Lab-5 | Quiz-2 | Solutions

--- Varsity-NSU/CSE311-Database/Lab5/Quiz2/50.Query.sql
+++ Varsity-NSU/CSE311-Database/Lab5/Quiz2/50.Query.sql
@@ -1,31 +1,24 @@
 
-// N.B: This is not the final version.
-
-// Q1.1
+/* Table 1 */
+//1.
 Select A.emp_name as "Name"
-From Employee A
-Where A.salary > (
-    select salary
-    From Employee B
-    Where A.mngr_id = B.emp_id
-)
+From Employee A, Employee B
+where (A.mngr_id = B.emp_id and A.salary > B.salary)
 
 //2.
-Select E.emp_name
-From Employee E, Employee EE
-Where NOT (E.mngr_id = EE.mngr_id and EE.dept_id = E.dept_id)
+Select A.emp_id, A.emp_name
+From Employee A, Employee B
+Where (A.mngr_id = B.emp_id and not A.dept_id = B.dept_id)
 
 
 //3.
 Select *
 From Employee E, Department D
-Where (hire_date LIKE '2017%' and E.dept_id = D.dept_id)
+Where (hire_date LIKE '2017%') and (E.dept_id = D.dept_id and D.dept_name = 'CSE')
 
 
-
-////////////////////////////////////////
-
-//Q2.1
+/* Table 2 */
+//1.
 Select S.sname
 From Supliers S, Parts P, Catalog C
 Where (S.sid = C.sid and C.pid = P.pid and P.color = 'red')
@@ -36,33 +29,11 @@ From Supliers S, Parts P, Catalog C
 Where (S.sid = C.sid and C.pid = P.pid and (P.color = 'red' or P.color = 'green'))
 
 //3.
-Select S.sid
-From Supliers S, Parts P, Catalog C
-Where (S.sid = C.sid and C.pid = P.pid and (P.color = 'red' or P.color = 'green'))
+Select A.sid, B.sid
+From catalog A, catalog B
+Where (A.pid = B.pid and A.cost > B.cost)
 
 //4
 Select S.sid
 From Supliers S, Parts P, Catalog C
 Where (S.sid = C.sid and C.pid = P.pid and (P.color = 'red' or S.address = 'Dhaka'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
