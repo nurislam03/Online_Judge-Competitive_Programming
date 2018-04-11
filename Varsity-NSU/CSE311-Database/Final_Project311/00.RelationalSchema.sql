a4b19f04193573f45203632c846660da18dab846
Nur Islam <nurislam0333@gmail.com>
2018-04-11 11:14:33 +0600

    CSE311 Project ER Schema and Table

---
+++ Varsity-NSU/CSE311-Database/Final_Project311/00.RelationalSchema.sql
@@ -0,0 +1,40 @@
+// Entities Relational Schema
+
+Patient(patient_id,admission_date,first_name,middle_name,last_name,birth_date,mobile1,mobile2, present_streetnum,present_streetname,present_area,present_thana,present_district, permanent_streetnum,permanent_streetname,permanent_area,permanent_thana,permanent_district,profession,amount_deposited,ward_id)
+
+Ward(ward_id,name)
+
+Bed(bed_id,type,rent,status,ward_id)
+
+Nurse(nurse_id,first_name,middle_name,last_name,birth_date,appointment_date,supervisor_id, ward_id)
+
+Medicine(medicine_id,name,type,unit_price,quantity,manufacture_date,expiry_date)
+
+Prescription(patient_id,doctor_id,name_med,quantity,times_a_day)
+
+Doctor(doctor_id,first_name,middle_name,last_name,birth_date,appointment_date,specialist,ward_id,registrar_id)
+
+Patient_info(patient_id,height,weight,symptom1,symptom2,symptom3,symptom4,low_bp,high_bp,bk1,bk2,bk3,lh1,lh2,lh3,dn1,dn2,dn3,hobby_game,hobby_others,disease)
+
+
+
+//For relationships
+Admit(patient_id,bed_id)
+
+Med-Prescription(patient_id,doctor_id,medicine_id)
+
+
+
+//For multi-values
+Doc_Educational_Qualifiation(doctor_id,degree,institute,year,cgpa) 
+
+Doc_Experience(doctor_id,job_title,from,to,organization)
+
+Nurse_Educational_Qualifiation(nurse_id,degree,institute,year,cgpa) 
+
+Nurse_Experience(nurse_id,job_title,from,to,organization)
+
+
+
+
+//
